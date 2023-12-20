809b17d92e883da0bfbbf72f30eb745d93f3c535
Gabrieldjc <gabrieljc@protonmail.com>
2023-12-19 21:55:25 -0300

    Inserção das informações do cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String NomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        String Agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua conta: ");
+        int Numero = scanner.nextInt();
+
+        System.out.println("Digite seu saldo: ");
+        Double Saldo = scanner.nextDouble();
+        
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua Agência é "+ Agencia + ", conta " + Numero + ", e seu saldo " + Saldo + " já está disponível para saque.");
+        
+        scanner.close();
     }
 }
