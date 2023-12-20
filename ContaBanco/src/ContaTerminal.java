ef185e5df6738310bd73410621da4a2e4a68c282
Gabrieldjc <gabrieljc@protonmail.com>
2023-12-19 22:08:10 -0300

    Mudando as variáveis para o padrão recomendado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,18 +6,18 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Por favor, digite seu nome: ");
-        String NomeCliente = scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
 
         System.out.println("Por favor, digite o número da sua Agência: ");
-        String Agencia = scanner.next();
+        String agencia = scanner.next();
 
-        System.out.println("Por favor, digite o número da sua conta: ");
-        int Numero = scanner.nextInt();
+        System.out.println("Por favor, digite o número da sua Conta: ");
+        int numero = scanner.nextInt();
 
-        System.out.println("Digite seu saldo: ");
-        Double Saldo = scanner.nextDouble();
+        System.out.println("Digite seu Saldo: ");
+        Double saldo = scanner.nextDouble();
         
-        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua Agência é "+ Agencia + ", conta " + Numero + ", e seu saldo " + Saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua Agência é "+ agencia + ", conta " + numero + ", e seu saldo " + saldo + " já está disponível para saque.");
         
         scanner.close();
     }
